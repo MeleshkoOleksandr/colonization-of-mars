5f17b6edfa441d26bed16a70604da2ea5358fa51
Alex Meleshko <4882672@gmail.com>
2026-04-17 14:18:06 +0200

    Adding comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 
-export const metadata = { title: "Mars Survival", description: "NASA Exercise" };
+export const metadata = { title: "Space Survival", description: "NASA Exercise" };
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="it" className="bg-[#0a0a0a]"> 
